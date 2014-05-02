66321e9c3dbea5ac301b792356d7713856ea5b6e
Jon Swanson <jswan2@uw.edu>
2014-05-02 16:39:00 -0700

    updated widgets.php

--- inc/widgets.php
+++ inc/widgets.php
@@ -6,4 +6,15 @@
 //3) authors widget that contains an array of authors.  Name, phone and email are fields for each author
 
 
+function uw_story_register_widgets() {
+    if (!is_blog_installed()){
+        return;
+    }
+
+    register_widget('UW_Story_Social_Widget');
+}
+
+require(get_stylesheet_directory() . '/inc/widgets/uw_story_social_widget.php');
 require( get_stylesheet_directory() . '/inc/widgets/gallery.php' );
+
+add_action('widgets_init', 'uw_story_register_widgets');
