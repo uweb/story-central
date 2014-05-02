159846b1204ebdac3a37d2d9d2ddff912a65d927
Jon Swanson <jswan2@uw.edu>
2014-05-02 09:40:54 -0700

    added back-end skeleton

---
+++ inc/widgets.php
@@ -0,0 +1,6 @@
+<?php
+//widgets go here
+//
+//1) social widget that includes a social site as the title and a blurb appropriate for the platform
+//2) links widget that is an array of links to related things, as many as necessary, starting with one
+//3) authors widget that contains an array of authors.  Name, phone and email are fields for each author
