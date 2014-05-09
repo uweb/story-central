4c906c20a02ec8ff7de54456d300cb7f93bad575
Kilian <kilianf@uw.edu>
2014-05-09 16:21:32 -0700

    Additional styling

--- inc/post-type-story.php
+++ inc/post-type-story.php
@@ -54,7 +54,7 @@ class Story
           'menu_position' => 5,
           'show_in_nav_menus' => true,
           'register_meta_box_cb' => array( $this, 'add_meta_boxes' ),
-          'supports' => array( 'title', 'editor', 'excerpt' )
+          'supports' => array( 'title', 'editor', 'excerpt', 'thumbnail' )
         )
     );
 
