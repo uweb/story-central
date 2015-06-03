9d7bc487abf1210ad8965ea281078b09e4462229
Dane Odekirk <daneodekirk@gmail.com>
2015-06-03 11:10:15 -0700

    Search template.

--- search.php
+++ search.php
@@ -24,8 +24,25 @@
               get_template_part( 'content', 'archive' );
             endwhile;
           elseif ( $posts = get_the_pillar_posts() ) :
-            foreach ($posts as $post ) : the_post( $post );
-              get_template_part( 'content', 'archive' );
+            foreach ($posts as $post ) : setup_postdata( $post );
+          ?>
+            <?php the_date('F j, Y', '<p class="date">', '</p>'); ?>
+            <h1>
+              <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title() ?></a>
+            </h1>
+            <?php
+            if (get_option('show_byline_on_posts')) :
+            ?>
+            <div class="author-info">
+                <?php the_author(); ?>
+                <p class="author-desc"> <small><?php the_author_meta(); ?></small></p>
+            </div>
+            <?php
+            endif;
+            ?>
+              <?php the_excerpt(); ?>
+
+          <?php
             endforeach;
           else :
             echo '<h3 class=\'no-results\'>Sorry, no results matched your criteria.</h3>';
